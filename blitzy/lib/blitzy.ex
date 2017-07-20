094d8b9dcc943c8f1af6aa4a9f889c79e8227836
Albert Salim <albertlimca@gmail.com>
2017-07-20 13:04:57 +0800

    Blitzy runner

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -1,18 +1,48 @@
 defmodule Blitzy do
-  @moduledoc """
-  Documentation for Blitzy.
-  """
+  def run(n_workers, url) when n_workers > 0 do
+    worker_fun = fn -> Blitzy.Worker.start(url) end
 
-  @doc """
-  Hello world.
+    1..n_workers
+    |> Enum.map(fn _ -> Task.async(worker_fun) end)
+    |> Enum.map(&Task.await(&1, :infinity))
+    |> parse_results
+  end
 
-  ## Examples
+  defp parse_results(results) do
+    {successes, _failures} =
+      results
+      |> Enum.partition(fn x ->
+          case x do
+            {:ok, _} -> true
+            _ -> false
+          end
+        end)
 
-      iex> Blitzy.hello
-      :world
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
 
-  """
-  def hello do
-    :world
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    average_time = average(data)
+    longest_time = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts """
+    Total workers: #{total_workers}
+    Successful requests: #{total_success}
+    Failed requests: #{total_failure}
+    Average (ms): #{average_time}
+    Longest (ms): #{longest_time}
+    Shortest (ms): #{shortest_time}
+    """
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
   end
 end
